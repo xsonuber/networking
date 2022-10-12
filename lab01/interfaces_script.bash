23d5750ca7dbabf4d3399b816fc2227d4ddf775f
xsonuber <71015964+xsonuber@users.noreply.github.com>
2022-10-12 12:40:42 +0300

    Add files via upload

---
+++ lab01/interfaces_script.bash
@@ -0,0 +1,48 @@
+#!/bin/bash
+#restoring settings
+sudo dhclient -r
+#echo default settings
+echo -e "\033[1mSettings: \033[0m"
+sudo echo "nameserver 192.168.0.1" | sudo tee /etc/resolv.conf
+sudo ifconfig enp0s3
+sudo route -n
+
+#cases -a for "auto" (using dhcp), -s for "static" (user enters the settings) and * for the wrong flag case
+
+echo -e "\033[1mYou chose: \033[0m"
+
+case "$1" in
+-a) echo "auto" 
+
+echo -e "\033[1mSettings after changes: \033[0m"
+
+sudo cat /etc/resolv.conf
+sudo dhclient enp0s3
+;;
+
+-s) echo "static" 
+
+echo -e "\033[1mEnter IP: \033[0m"
+read ip1
+echo -e "\033[1mEnter netmask: \033[0m"
+read mask1
+echo -e "\033[1mEnter gateway: \033[0m"
+read gateway1
+echo -e "\033[1mEnter DNS: \033[0m"
+read dns1
+
+#changing settings
+
+sudo ifconfig enp0s3 $ip1 netmask $mask1
+sudo route add default gw $gateway1 enp0s3
+echo -e "\033[1mSettings after changes: \033[0m"
+
+sudo echo "nameserver $dns1" | sudo tee /etc/resolv.conf
+;;
+
+*) echo "error" ;;
+esac
+
+sudo ifconfig enp0s3
+sudo route -n
+
